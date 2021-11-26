de3a5ba3ac18d0ef530982dc1d21246d79eed36c
Anjali Chauhan <56559378+anjali1102@users.noreply.github.com>
2021-11-26 19:28:26 +0530

    Updated  script.js

--- mock2/password checker/script.js
+++ mock2/password checker/script.js
@@ -1,27 +1,31 @@
 var userPassword=document.querySelector('#input')
 var statusPassword=document.querySelector('#output')
 var buttonCheck=document.querySelector('#btn')
+buttonCheck.disabled=true
 
 function checkHandler(e){
     let inputComing=e.target.value;
     if(inputComing.length>0 && inputComing.length<10){
         message="SUCCESS"
-        statusPassword.innerHTML=message;
-        buttonCheck.disabled=true;
-
+        statusPassword.innerHTML=message;    
     }
     else if(inputComing.length===0){
         message="ENTER INPUT"
         statusPassword.innerHTML=message
-        buttonCheck.disabled=true;
     }
     else if(inputComing.length>10){
         message="ERROR"
         statusPassword.innerHTML=message;
-        buttonCheck.disabled=false;
     }
-
-
 }
 
+function BtnActivityHandler(e){
+    let inputComing=e.target.value;
+    if(inputComing.length>10){
+        buttonCheck.disabled=false
+    }
+}
+
+
 userPassword.addEventListener('input', checkHandler)
+userPassword.addEventListener('input',BtnActivityHandler)
