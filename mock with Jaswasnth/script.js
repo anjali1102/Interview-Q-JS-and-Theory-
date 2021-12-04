7bf323d920e75f45b3b88cb1a43771f54ad46c24
Anjali Chauhan <56559378+anjali1102@users.noreply.github.com>
2021-12-04 17:24:42 +0530

    Update script.js

--- mock with Jaswasnth/script.js
+++ mock with Jaswasnth/script.js
@@ -1,17 +1,39 @@
-var button=document.querySelector('#btn')
-var output=document.querySelector('.app')
-var URL="https://randomuser.me/api/?results=10"
+var button = document.querySelector("#btn");
+var output = document.querySelector(".output");
+
+var URL = "https://randomuser.me/api/?results=10";
+
+
 function fetchHandler(){
     fetch(URL)
-     .then(response => response.json())
-     .then(data => 
-     data.results.forEach(element => {
-        // console.log(element.name.first)
-        // output.innerText=element.name.first+
-        var newEle=document.createElement("div")
-        newEle.append(div)
-     })
-    )
+    .then(response => response.json())
+    .then(data => {
+        var resultsArr = data.results;
+        // console.log(data);
+
+        resultsArr.map(each => {
+            var divContainer = document.createElement("div");
+            var pName = document.createElement("p");
+            var pAge = document.createElement("p");
+
+            pName.innerText = each.name.first;
+            pAge.innerText = each.dob.age;
+
+            if(each.dob.age >= 45){
+                pName.style.color = "orange";
+            }
+
+
+            divContainer.append(pName);
+            divContainer.append(pAge);
+
+            output.append(divContainer);
+            console.log(each.name.first, each.dob.age);
+        })
+
+
+    })
 }
 
-button.addEventListener("click", fetchHandler)
+
+button.addEventListener("click", fetchHandler);
